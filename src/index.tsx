b5f1fa9929dada4833d47f2ec102fab5014a78f7
Soulike <soulike@qq.com>
2022-05-11 20:46:36 +0800

    修复 bug
    - 回滚 index.tsx 修复 react-router v5 与 React 18 的不兼容

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'core-js';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './ModuleConfig/Antd';
 import zhCN from 'antd/es/locale/zh_CN';
@@ -8,13 +8,14 @@ import {ConfigProvider} from 'antd';
 import './GlobalStyle';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+// React Router V5 在新的 render API 下 useLocation 似乎存在 bug（体现在侧边栏标签的选择上），在升级之前要测试
+ReactDOM.render(
     <React.StrictMode>
         <ConfigProvider locale={zhCN}>
             <App />
         </ConfigProvider>
-    </React.StrictMode>);
+    </React.StrictMode>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
