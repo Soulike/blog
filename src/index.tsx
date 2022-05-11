29e288a18d33d28e6bfc5dffc38edc70089b1e0e
Soulike <soulike@qq.com>
2022-05-11 20:10:45 +0800

    升级依赖，修改配置
    - index.tsx 根据 React 18 的特性进行了修改
    - 删除了 craco 和 esbuild
    - 升级依赖

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'core-js';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
 import './ModuleConfig/Antd';
 import zhCN from 'antd/es/locale/zh_CN';
@@ -8,11 +8,13 @@ import {ConfigProvider} from 'antd';
 import './GlobalStyle';
 import App from './App';
 
-ReactDOM.render(
-    <ConfigProvider locale={zhCN}>
-        <App />
-    </ConfigProvider>,
-    document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <React.StrictMode>
+        <ConfigProvider locale={zhCN}>
+            <App />
+        </ConfigProvider>
+    </React.StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
